bad5a60e9b650fc840770b277899d853ec7e0300
Ladislav Thon <ladicek@gmail.com>
2012-02-02 23:03:33 +0100

    changed 2011 to 2012

--- dpm.dart
+++ dpm.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, Ladislav Thon. All rights reserved. Use of this source
+// Copyright (c) 2012, Ladislav Thon. All rights reserved. Use of this source
 // code is governed by a BSD-style license that can be found in the LICENSE file.
 
 #library('dpm');
